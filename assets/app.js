2df657a2cd05b45466eba18271b92d978d996cb1
chris-hildebrandt <ch.hild15@gmail.com>
2022-07-21 17:17:38 -0600

    lab

---
+++ assets/app.js
@@ -0,0 +1,19 @@
+const = dictionary{
+
+}
+
+function drawDictionary(){
+  let template = ''
+  for(let key in dictionary){
+    let item = dictionary[key]
+    template += ``
+    let divElm = document.getElementById("section id")
+    divElm.innerHTML = template
+  }
+}
+
+switch(item we are looking at){
+  case 'something' : somethingElse += 10
+  break;
+  
+}
