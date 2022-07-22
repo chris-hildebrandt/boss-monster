270ebe567f0c7cfce862e8c7cf3cbe3e01e1844d
chris-hildebrandt <ch.hild15@gmail.com>
2022-07-21 21:33:09 -0600

    homework

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,21 @@
-const = dictionary{
-to access an object as a variable you need to use [] otherwise it will look for a literal .variable which doesn't exist.
-}
+// const = dictionary{
+// to access an object as a variable you need to use [] otherwise it will look for a literal .variable which doesn't exist.
+// }
 
-function drawDictionary(){
-  let template = ''
-  for(let key in dictionary){
-    let item = dictionary[key]
-    template += ``
-    let divElm = document.getElementById("section id")
-    divElm.innerHTML = template
-  }
-}
+// function drawDictionary(){
+//   let template = ''
+//   for(let key in dictionary){
+//     let item = dictionary[key]
+//     template += ``
+//     let divElm = document.getElementById("section id")
+//     divElm.innerHTML = template
+//   }
+// }
 
-switch(item we are looking at){
-  case 'something' : somethingElse += 10
-  break;
+// switch(item we are looking at){
+//   case 'something' : somethingElse += 10
+//   break;
   
-}
+// }
+
+const boss 
